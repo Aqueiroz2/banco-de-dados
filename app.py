babf2a138a7cbafdb069c206e5b11829a3f899b8
Aqueiroz2 <40298581+Aqueiroz2@users.noreply.github.com>
2025-01-27 10:02:38 -0400

    codigo finalizado

--- app.py
+++ app.py
@@ -1,30 +1,68 @@
 import sqlite3
 
-conexao = sqlite3.connect('funcionarios.db')
 
-cursor = conexao.cursor()
-cursor.execute("""
-    CREATE TABLE IF NOT EXISTS Funcionarios (
-        id INTEGER PRIMARY KEY,
-        nome TEXT NOT NULL,
-        cargo TEXT NOT NULL,
-        dataContratacao TEXT NOT NULL
-    );
-""")
-conexao.commit()
+def conectar_db(nome_db):
+    conexao = sqlite3.connect(nome_db)
+    return conexao
+
+# Criando Tabela
+def criar_tabela(conexao):
+    cursor = conexao.cursor()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS Funcionarios (
+            id INTEGER PRIMARY KEY,
+            nome TEXT NOT NULL,
+            cargo TEXT NOT NULL,
+            dataContratacao TEXT NOT NULL
+        );
+    """)
+    conexao.commit()
+
+# Inserir dados em uma tabela
+def inserir_funcionario(conexao, id, nome, cargo, dataContratacao):
+    cursor = conexao.cursor()
+    cursor.execute("INSERT INTO Funcionarios VALUES (?, ?, ?, ?)",
+                   (id, nome, cargo, dataContratacao))
+    conexao.commit()
+
+# Listar todos os dados da tabela
+def selecionar_todos_funcionarios(conexao):
+    cursor = conexao.cursor()
+    cursor.execute("SELECT * FROM Funcionarios")
+    return cursor.fetchall()
+
+# Alterar dados de uma tabela
+def atualizar_funcionario(conexao, id, cargo):
+    cursor = conexao.cursor()
+    cursor.execute("UPDATE Funcionarios SET cargo = ? WHERE id = ?", (cargo, id))
+    conexao.commit()
+
+# Excluir dados de uma tebela
+def deletar_funcionario(conexao, id):
+    cursor = conexao.cursor()
+    cursor.execute("DELETE FROM Funcionarios WHERE id = ?", (id,))
+    conexao.commit()
+
+conexao = conectar_db('funcionarios.db')
+
+criar_tabela(conexao)
 
 nome = input('Digite seu nome: ')
-cargo = input('Digite um cargo: ')
-data = input('Digite uma data no formato aaaa-mm-dd: ')
+cargo = input('Informe o cargo: ')
+data = input('Digite a data no formato aaaa-mm-dd')
 
-cursor.execute("INSERT INTO Funcionarios VALUES (?, ?, ?, ?)",
-               (1, nome, cargo, data))
-conexao.commit()
+inserir_funcionario(conexao, 1, nome, cargo, data)
+inserir_funcionario(conexao, 2, nome, cargo, data)
 
+print(selecionar_todos_funcionarios(conexao))
 
-nome = input('Digite seu nome: ')
-cargo = input('Digite um cargo: ')
-data = input('Digite uma data no formato aaaa-mm-dd: ')
-cursor.execute("INSERT INTO Funcionarios VALUES (?, ?, ?, ?)",
-               (2, nome, cargo, data))
-conexao.commit()
+atualizar_funcionario(conexao, 1, 'Desenvolvedor Sênior')
+
+print(selecionar_todos_funcionarios(conexao))
+
+deletar_funcionario(conexao, 1)
+
+print(selecionar_todos_funcionarios(conexao))
+
+conexao.close()
+
